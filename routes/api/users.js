ac66332c71aa316c13fea50537982dcebd535cf9
Axel Roffi <axel.roffi@gmail.com>
2018-05-08 12:27:12 +0200

    user registration route bcrypt body parser gravatar library

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
+const bcrypt = require("bcrypt");
 
 //Load User model
 const User = require("../../models/User");
@@ -14,12 +15,12 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 //@route GET api/users/register
 //@desc register user
 //@access Public
-router.post("register", (req, res) => {
+router.post("/register", (req, res) => {
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
     } else {
-      const avatar = gravatar.url(req.bod.emai, {
+      const avatar = gravatar.url(req.bod.email, {
         s: 200, //size in pixel
         r: "pg", // rating
         d: "mm" //Default
@@ -27,8 +28,19 @@ router.post("register", (req, res) => {
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
-        avatar: avatar,
+        avatar,
         password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
       });
     }
   });
